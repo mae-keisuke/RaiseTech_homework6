d0e6f08b84a0185a7c9ea8fef7b491fb0445fce7
MAE <dup0729@gmail.com>
2023-02-23 19:25:23 +0900

    SpringによるHelloWorldの実装

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.helloworld.controller;
 
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.time.LocalDateTime;
+
+@RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "tmp_hello";
+        return "Hello World";
     }
+
 }
