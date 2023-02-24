8dabeee90e9f82738e4f61e1dcc6e2c71bf90445
MAE <dup0729@gmail.com>
2023-02-24 14:30:50 +0900

    現在時刻表示及び国別挨拶取得機能追加

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -1,15 +1,41 @@
 package com.example.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World";
     }
 
+    @GetMapping("/now")
+    public String time() {
+        LocalDateTime now = LocalDateTime.now();
+        int hour = now.getHour();
+        int minutes = now.getMinute();
+        int sec = now.getSecond();
+        return hour + "時" + minutes + "分" + sec + "秒";
+    }
+
+    @GetMapping("/greeting")
+    public String country(@RequestParam("country") String country) {
+        Map<String, String> countryLanguage = new HashMap<>();
+        countryLanguage.put("Japan", "こんにちは！");
+        countryLanguage.put("America", "Hello！");
+        countryLanguage.put("France", "Bonjour！");
+        countryLanguage.put("Italy", "Ciao！");
+
+        if (countryLanguage.get(country) == null) {
+            return "リストにありません。再検索してください。";
+        } else {
+            return countryLanguage.get(country);
+        }
+    }
 }
