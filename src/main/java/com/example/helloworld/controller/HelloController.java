9aef7b47dd29759ffd31b1f92a6de4f5918cc071
MAE <dup0729@gmail.com>
2023-02-26 14:34:16 +0900

    checkstyle動作確認

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 public class HelloController {
-    private static final Map<String, String> COUNTRY_TO_LANGUAGE = Map.of("Japan", "こんにちは", "America", "Hello！", "France", "Bonjour！", "Italy", "Ciao！");
+    private static final Map<String, String> Country_TO_LANGUAGE = Map.of("Japan", "こんにちは", "America", "Hello！", "France", "Bonjour！", "Italy", "Ciao！");
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH時mm分ss秒");
 
     @GetMapping("/hello")
@@ -27,8 +27,8 @@ public class HelloController {
 
     @GetMapping("/greeting")
     public String country(@RequestParam("country") String country) {
-        if (COUNTRY_TO_LANGUAGE.containsKey(country)) {
-            return COUNTRY_TO_LANGUAGE.get(country);
+        if (Country_TO_LANGUAGE.containsKey(country)) {
+            return Country_TO_LANGUAGE.get(country);
         } else {
             return "リストにありません。再検索してください。";
         }
